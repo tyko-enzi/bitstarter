3a92e5e64337be4313fe098abbf3e68f215e6339
tyko enzi <r.kaleb72@gmail.com>
2013-07-31 10:31:52 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync("index.html")));
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
